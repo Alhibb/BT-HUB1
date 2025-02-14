b50a708fe9585fc02fd95870519ecda3d9b6f7eb
Alhibb <63309522+Alhibb@users.noreply.github.com>
2025-02-14 18:09:18 +0100

    Smth

---
+++ class1.py
@@ -0,0 +1,8 @@
+suna1 = "Halima"
+suna2 = "Maryam"
+age = 14
+age2 = 13
+print("My name is", suna1.upper(), "and i am ", age, "years old")
+print("3 x 1 = ", 3*1)
+print("3 x 2 = ", 3*2)
+print("3 x 3 = ", 3*3)
